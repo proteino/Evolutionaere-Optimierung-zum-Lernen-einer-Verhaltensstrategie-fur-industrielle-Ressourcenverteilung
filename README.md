1aed3625a9a8e67425e9dcd1d7b21871ebbf06d7
Jost <79093597+proteino@users.noreply.github.com>
2023-09-14 15:13:39 +0200

    added user instructions

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Evolutionaere Optimierung zum Lernen einer Verhaltensstrategie für industrielle Ressourcenverteilung
+
+zur Ausführung des Codes bitte die folgende virtual environment nutzen: TODO
+
+Mit der Datei main.py wird die Optimierung einer Verhaltensstrategie für eine ContainerGym Umgebung mit fünf Containern und zwei PUs gestartet. Die Lösung des Optimierers sowie eine grafische Evaluation sind im Ordner "results" zu finden. Dort ist beispielhaft bereits das Ergebnis einer Optimierung für eine Umgebung mit einem Container und einer PU hinterlegt. 
+
+Falls sie eigene Experimente durchführen möchten, können sie die Variablen in der Datei main.py entsprechend anpassen.
  
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from optimization_routine import optimize_cma
 
 if __name__ == "__main__":
     # set parameters
-    config = "5containers_2proc_units"
-    n_hidden_layers = 1
-    n_neurons_hidden_layers = 5
-    step_size = 2 #initial step size
-    episodes_per_function_call = 1
+    config = "5containers_2proc_units" # if you want to use a different configuration you can use the examples in ContainerGym/contaienrgym/configs
+    n_hidden_layers = 1 # number of hidden layers of the artificial neural Network that implements the policy
+    n_neurons_hidden_layers = 5 # dimension of the hidden layers
+    step_size = 2 #initial step size of the cma-es optimizer
+    episodes_per_function_call = 1 # number of episodes the fitness function runs to calculate the average collected reward of the policy
+    factor_pop_size = 1 # Increases the population size of the optimizer by a factor. For Value one the default population size of cma-es is used
 
-    optimize_cma(config, n_hidden_layers, n_neurons_hidden_layers, step_size, episodes_per_function_call)
+    optimize_cma(config, n_hidden_layers, n_neurons_hidden_layers, step_size, episodes_per_function_call, factor_pop_size=factor_pop_size)
